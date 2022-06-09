fa6ca4b2c7f826f6d417a22e58c25098283de805
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-06-09 00:57:50 -0300

    feat: useMemo

--- components/SearchResults/index.tsx
+++ components/SearchResults/index.tsx
@@ -1,9 +1,18 @@
+import { useMemo } from "react";
 import { ProductItem } from "../ProductItem";
 import { SearchResultsProps } from "./types";
 
 export function SearchResults({ results }: SearchResultsProps) {
+  const totalValue = useMemo(() => {
+    return results.reduce((acc, product) => {
+      return acc + product.price;
+    }, 0);
+  }, [results]);
+
   return (
     <div>
+      <h2>{totalValue}</h2>
+
       {results.map(product => (
         <ProductItem key={product.id} product={product} />
       ))}
